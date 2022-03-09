8f462c8d9800e084796a678cebf8d05a97259864
Ajay Sarvasiddhi <Ajay Sarvasiddhi@LAPTOP-QI35KH5D>
2022-03-09 15:00:10 +0530

    TC - O(N), SC - O(1)

---
+++ Util/src/com/leetcode/arrays/ValueOfVariableAfterPerformingNOperations.java
@@ -0,0 +1,22 @@
+package com.leetcode.arrays;
+
+public class ValueOfVariableAfterPerformingNOperations {
+
+	public static int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        
+        for(String op : operations) {
+            if(op.toUpperCase().equals("X++") || op.toUpperCase().equals("++X")) {
+                x += 1;
+            } else if(op.toUpperCase().equals("X--") || op.toUpperCase().equals("--X")){
+                x -= 1;
+            }
+        }
+        
+        return x;
+    }
+	
+	public static void main(String[] args) {
+		
+	}
+}
